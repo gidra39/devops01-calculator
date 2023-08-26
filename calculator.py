fafb7bd3fba5ca7f0e9fa1eda61752429071a231
gidra <95176865+gidra39@users.noreply.github.com>
2023-08-26 17:25:00 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,27 @@
-def calculator(operation, input1, input2):
+def calculator(operation, float_num1, float_num2):
 
     if operation == '1':
-        x = input1 + input2
+        x = float_num1 + float_num2
         print(x) 
     elif operation == '2':
-        x = input1 - input2
+        x = float_num1 - float_num2
         print(x) 
     elif operation == '3':
-        x = input1 * input2
+        x = float_num1 * float_num2
         print(x)    
     elif operation == '4':
-        x = input1 // input2    
+        x = float_num1 // float_num2    
         print(x)    
     else:
         print("Invalid operator. Please enter a valid operation")
         exit()
     
 try:
-    input1_user = input("Enter a number: ")
-    input2_user = input("Enter a number: ")
-    int_input1_user = float(input1_user)
-    int_input2_user = float(input2_user)
+    float_num1 = float(input("Enter first number: "))
+    float_num2 = float(input("Enter second number: "))
 except ValueError:
     print("Invalid input. Please enter a valid number.")
     exit()
 operation_user = input("Enter an operation (1 - addition, 2 - subtraction, 3 - multiplication or 4 - dividing): ")
-calculator(operation_user, int_input1_user, int_input2_user)
+calculator(operation_user, float_num1, float_num2)
 
